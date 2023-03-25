9d0959679a18e90296847064c7ff2758d76074bc
basilkiwanuka <basilkiwanuka@icloud.com>
2023-03-25 16:35:19 -0400

    Initial commit

---
+++ src/test/java/com/mbsystems/securitylesson01/SecurityLesson01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbsystems.securitylesson01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLesson01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
